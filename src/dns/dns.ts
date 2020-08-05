10aaca12f2d5f4154574c0975f6fa1a321a5ceef
Ionescu Alex <alx.ionescu97@gmail.com>
2020-08-05 19:03:06 +0300

    Even better debugging

--- src/dns/dns.ts
+++ src/dns/dns.ts
@@ -3,6 +3,9 @@ import dns, { MxRecord } from 'dns'
 export const getMx = async (domain: string): Promise<dns.MxRecord[]> => {
   return new Promise(r =>
     dns.resolveMx(domain, (err, addresses) => {
+      if (err) {
+        console.error(domain, err)
+      }
       if (err || !addresses) return r([] as dns.MxRecord[])
       r(addresses.sort((lhs: MxRecord, rhs: MxRecord) => lhs.priority - rhs.priority))
     })
